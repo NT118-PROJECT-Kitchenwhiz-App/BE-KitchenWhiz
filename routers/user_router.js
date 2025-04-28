3d9c3a05303e611b373e65cd2c0d33e772dc19cf
trthuytien <tranthithuytien0615@gmail.com>
2025-04-28 12:17:07 +0700

    feat: forgot password

--- routers/user_router.js
+++ routers/user_router.js
@@ -4,5 +4,7 @@ const UserController = require("../controllers/user_controller");
 router.post('/registration', UserController.register);
 router.post('/login', UserController.login);
 router.post('/vertifyOtp', UserController.vertifyOtp);
+router.post('/forgotPassword', UserController.forgotPassword);
+router.post('/resetPassword', UserController.resetPassword);
 
 module.exports = router;
